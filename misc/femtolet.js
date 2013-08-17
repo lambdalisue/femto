bb98b18e21e3e364d88330dc8175a14b48edbcb1
Λlisue <lambdalisue@hashnote.net>
2013-08-17 21:24:44 +0900

    Fix bookmarklet and add info

--- misc/femtolet.js
+++ misc/femtolet.js
@@ -2,6 +2,8 @@ javascript:(function(){
     var s = document.createElement('script');
     s.src="http://goo.gl/NHDBkx";
     s.onload = function(){
+        console.log("Loaded");
         Femto.transformAll();
-    };
+    };          
+    document.body.appendChild(s);
 })();
